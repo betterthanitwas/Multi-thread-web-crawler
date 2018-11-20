ccbca6383caa25ddef42c09e327074cc089cc62a
Jacob Winters <jacobwinters@users.noreply.github.com>
2018-11-20 12:35:14 -0700

    Add database interface skeleton

---
+++ datastore.py
@@ -0,0 +1,6 @@
+class DataStore:
+    # Takes two strings and a dictionary that goes from strings to ints
+    def indexPage(url, title, wordCounts): pass
+
+    # Takes a list of words, returns a list of (url, title) tuples
+    def search(words): pass
