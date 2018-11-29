4d79bbc4131c2ad3c50d58f3856e5c5336120159
Ryan Pacheco <Ryan@Ryans-MacBook-Air.local>
2018-11-28 22:02:14 -0700

    this is the web scraper that respects robots.txt, only scrapes for page title, url, and other links, not text yet.

--- bs_test.py
+++ bs_test.py
@@ -1,20 +1,30 @@
 from bs4 import BeautifulSoup
 
 import requests
+import urllib.robotparser
+
+
+
 
 url = input("Enter a website to extract the URL's from: ")
 r  = requests.get(url)
 data = r.text
-soup = BeautifulSoup(data)
-
-
-
-
-#print(soup.get_text("you"))
-
+soup = BeautifulSoup(data, features="html.parser")
+word_dic = {}
 links = []
+title = soup.title.string
 
+print(title)
+print(url)
+rp = urllib.robotparser.RobotFileParser()
+rp.set_url((url + "/robots.txt"))
+rp.read()
 for link in soup.find_all('a'):
-    links.append(link.get('href'))
+    next_link = link.get('href')
+    if next_link != None:
+        if rp.can_fetch("*", next_link):
+            links.append(next_link)
+            print(next_link)
 
-print(links[7])
+#print(links)
+
