2d088c741435bb59ca51c4a7f441fd0178bab7b3
cplewis <caseylewis@nnu.edu>
2018-12-01 10:53:30 -0700

    Removed connect method
    Fixed conn so it is a local variable, also changed how connect pool is implemented to ensure a connection isn't made and not used.

--- datastore.py
+++ datastore.py
@@ -4,72 +4,48 @@ from python_mysql_dbconfig import read_db_config
 import threading
 
 class DataStore:
-	def __init__(self, configFile):
+	def __init__(self, config_file):
 		'''
 		Example:
 			ds = DataStore('config.ini')
-			ds.connect()
 
 			url = 'www.Thisisatest.com'
 			title = 'Test Title'
-			dictWord = {'test1': (50, "this test1 word is a test excerpt"),
+			dict_word = {'test1': (50, "this test1 word is a test excerpt"),
 						'test2': (10, "this test2 : excerpt")}
-			ds.indexPage(url, title, dictWord)
+			ds.indexPage(url, title, dict_word)
 
-			wordList = ['test1', 'test2']
-			r = ds.search(wordList)
+			word_list = ['test1', 'test2']
+			r = ds.search(word_list)
 			for t in r:
 				print(t)
-		
-			ds.closeConnection()
 		'''
-		self.db_config = read_db_config(configFile)
+		# holds configuration data (user, password, etc) for db connection
+		self.db_config = read_db_config(config_file)
 
-
-	def connect(self):
-		""" 
-			Connects to database
-		"""
-		try:
-			# most connections allowed to Database
-			maxconnections = 5
-
-			# create counting semaphore 
-			self.pool_sema = threading.BoundedSemaphore(value = maxconnections)
-
-			# create connection pool implicitly
-			self.conn = mysql.connector.connect(pool_name="thePool",
-												pool_size=maxconnections, # 5 is the default
-												**self.db_config)
-			if self.conn.is_connected():
-				print('connection established.')
-			else:
-				print('connection failed.')
-
-			# turn off autocommit
-			self.conn.autocommit = False
-
-
-		except mysql.connector.Error as err:
-			if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-				print("Something is wrong with your user name or password")
-			elif err.errno == errorcode.ER_BAD_DB_ERROR:
-				print("Database does not exist")
-			else:
-				print(err)
+		# most connections allowed to Database and semaphore
+		self.max_connections = 5
+		# create counting semaphore 
+		self.pool_sema = threading.BoundedSemaphore(value = self.max_connections)
 
 
 	# (url, title, Dict<word: (wordCount, excerpt)>)
 	def indexPage(self, url, title, words):
 		""" Inputs are a url (string), title (string), Dict<word (string), tuple(wordCount (int), excerpt(string)>
 		"""
-
 		# with automatically calls acquire and release upon entering and exiting block
 		with self.pool_sema:
-			# grab connection from pool
-			self.conn = mysql.connector.connect(pool_name="thePool")
+
+			# use connection pool implicitly
+			conn = mysql.connector.connect(pool_name="thePool",
+												pool_size=self.max_connections, # 5 is the default
+												**self.db_config)
 
 			try:
+				
+				# turn off autocommit
+				conn.autocommit = False
+
 				# check lengths: URL <= 255 title <= 100 
 				url = str(url[0:255])
 				title = str(title[0:100])
@@ -80,43 +56,41 @@ class DataStore:
 				# start a transaction
 				''' SERIALIZABLE is restrictive and does nont all dirty, nonrepeatable or phantom reads
 				 might be able to go to less restrictive... '''
-				self.conn.start_transaction(isolation_level='SERIALIZABLE')
+				conn.start_transaction(isolation_level='SERIALIZABLE')
 
 				# create a cursor to call url procedure
-				cursorUrl = self.conn.cursor()
+				cursor_url = conn.cursor()
 
 				# results holds the args sent into callproc but contains an output with the uid
 				# that is needed in the next mysql procedure call
-				results = cursorUrl.callproc('PRC_STORE_URL_TTL', args)
-				
-				''' NEXT VERSION HAVE MYSQL ITERATE THROUGH LIST OF WORDS '''
+				results = cursor_url.callproc('PRC_STORE_URL_TTL', args)
 
 				# create a cursor to call store word procedure
-				cursorWord = self.conn.cursor()
+				cursor_word = conn.cursor()
 
 				# store word, wordcount and excerpt
-				for word, wordData in words.items():
+				for word, word_data in words.items():
 					# check lengths: Word <= 45, excerpt <= 150
 					word = str(word[0:45])
-					excerpt = str(wordData[1][0:150])
+					excerpt = str(word_data[1][0:150])
 					
 					# store word id, word, word count and excerpt into db
-					args = (word, results[2], wordData[0], excerpt)
-					cursorWord.callproc('PRC_STORE_WRD', args)
+					args = (word, results[2], word_data[0], excerpt)
+					cursor_word.callproc('PRC_STORE_WRD', args)
 					
 				# call commit
-				self.conn.commit()
+				conn.commit()
 
 				# close cursors
-				cursorUrl.close()
-				cursorWord.close()
+				cursor_url.close()
+				cursor_word.close()
 
 
 			except mysql.connector.Error as err:
 				print(err)
 			
 			# return connection back to pool
-			self.conn.close()
+			conn.close()
 			
 
 	# List<word> -> List<(url, title, excerpt, word count)>
@@ -130,42 +104,34 @@ class DataStore:
 
 		# with automatically calls acquire and release upon entering and exiting block
 		with self.pool_sema:
-			# grab connection from pool
-			self.conn = mysql.connector.connect(pool_name="thePool")
 
-			retList = []
+			# use connection pool implicitly
+			conn = mysql.connector.connect(pool_name="thePool",
+												pool_size=self.max_connections, # 5 is the default
+												**self.db_config)
+
+			return_list = []
 
 			try:
-
-				
-				cursorRetrieve = self.conn.cursor()
+				# create a cursor to call search procedure
+				cursor_retrieve = conn.cursor()
 
 				for word in words:
 					# find word in database and return a row
-					cursorRetrieve.callproc('PRC_FIND_WRD', (word,))
+					cursor_retrieve.callproc('PRC_FIND_WRD', (word,))
 
 					# add result to return list 
-					for result in cursorRetrieve.stored_results():
+					for result in cursor_retrieve.stored_results():
 						#print(result.fetchall())
-						retList.append(tuple(result))
+						return_list.append(tuple(result))
 
-				''' DO THIS SECOND
-				# pass list of words to database
-				args[words]
-				self.cursor.callproc('PRC_FIND_WORD', args)
-
-				# add result to return list 
-				for result in cursor.stored_results():
-					print(result.fetchall())
-					#retList.append(result)
-				'''
 				# close cursor
-				cursorRetrieve.close()
+				cursor_retrieve.close()
 
 			except mysql.connector.Error as err:
 				print(err)
 		
 			# return connection back to pool
-			self.conn.close()
+			conn.close()
 
-			return retList
+			return return_list
