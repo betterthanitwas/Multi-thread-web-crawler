81a2fc40ac6c8e720ecaff683e694b43b7c01f21
Kyle Pacheco <kylepacheco@Kyles-MacBook-Air.local>
2018-12-04 14:17:44 -0700

    CSS STUFF

--- renderhtml.py
+++ renderhtml.py
@@ -8,23 +8,36 @@ def render_html(results, search, search_words):
     return f"""<!doctype html>
 <title>CLocKPJWaRP: {escape(search)}</title>
 <style>
-h1 {{
+h1 {
 	margin: 0;
-}}
-h2 {{
+	font-family: monaco;
+}
+h2 {
 	font-size: 1.25em;
 	margin-bottom: 0;
-}}
-p {{
+}
+p {
 	margin-top: 0;
-}}
+}
+
+input{
+	border-radius: 15px;
+	border: solid;
+	border-width: thin;
+}
+
+#button{
+	background-color: blue;
+	color: white;
+}
+
 </style>
 <link rel="search" type="application/opensearchdescription+xml" title="CLocKPJWaRP" href="opensearch.xml">
 
 <h1>CLocKPJWaRP</h1>
 <form action="search" method="get">
 	<input type="search" name="q" value="{escape(search)}">
-	<input type="submit" value="Find it">
+	<input type="submit" value="Find it" id="button">
 </form>
 """ + '\n'.join(f'''<h2><a href="{escape(str(url, encoding="UTF-8"))}">{bold_search_terms(escape(str(title, encoding="UTF-8")))}</a></h2>
 <p>{bold_search_terms(escape(str(excerpt, encoding="UTF-8")))}</p>''' for (url, title, excerpt) in results)
